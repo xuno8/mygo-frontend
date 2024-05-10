4eb219c4a664cf73a54b868066cd0c2fd9abe520
xuno <timlin0108@gmail.com>
2024-05-11 02:30:09 +0800

    install vuex and click 'logo' to home

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="bg-transparent fixed top-0 left-0 w-full">
     <div class="container mx-auto px-4 py-4 flex items-center">
-      <img src="@/assets/logo.png" alt="Logo" class="h-10" />
+      <router-link to="/">
+        <img src="@/assets/logo.png" alt="Logo" class="h-10" />
+      </router-link>
     </div>
   </nav>
 </template>
